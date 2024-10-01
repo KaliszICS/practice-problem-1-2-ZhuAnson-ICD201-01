14ad52a885fea447b4f4c4f5e744855528a94beb
ZhuAnson-ICD201-01 <anson.zhu@student.tdsb.on.ca>
2024-10-01 14:02:47 +0000

    asd

--- PP1_2.py
+++ PP1_2.py
@@ -2,36 +2,36 @@ def q1():
     name="Alex"
     print(name)
 
-def q2()
+def q2():
     num1=3
     num2=2
-    num3=4
-    print(f"sum of all numbers is {num1+num2+num3}")
+    num3=5
+    print(num1+num2+num3)
 
-def q3()
+def q3():
     num1=21
     num2=4
-    print(f"the answer is {num1//num2}")
+    print(num1//num2)
 
-def q4()
+def q4():
     num1=6.3
     num2=3.0
-    print(f"the answer is {num1/num2}")
+    print(num1/num2)
 
-def q5()
+def q5():
     word="Hello World"
     print(word)
     word="Goodbye World"
     print(word)
 
-def q6()
+def q6():
     value=True
     print(value)
 
-def q7()
+def q7():
     num1=7
     num2=2 
-    print(f"remainder of the two numbers is {num1%num2}")
+    print(num1%num2)
 
 # q1()
 # q2()
